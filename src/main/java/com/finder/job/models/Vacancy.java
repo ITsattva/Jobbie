2c0c77de9e3e2b6a338c4e0c4b59a4dc5c4eb62a
itsattva <butthisattva@gmail.com>
2022-10-10 15:49:19 +0300

    closes #26

--- src/main/java/com/finder/job/models/Vacancy.java
+++ src/main/java/com/finder/job/models/Vacancy.java
@@ -3,10 +3,14 @@ package com.finder.job.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class Vacancy {
     private String title;
     private String description;
     private String salary;
+    private String site;
+    private LocalDateTime dateTime;
 }
